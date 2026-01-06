edb133665b2611124a9f4a1fc949aeaecdf84ae5
Juliana Araújo <julianaraujo555@outlook.com>
2026-01-05 22:07:51 -0300

    Update refresh_token.py

--- refresh_token.py
+++ refresh_token.py
@@ -3,23 +3,23 @@ import json
 import os
 from datetime import datetime
 
+print("ENV OK:", os.environ.get("ML_CLIENT_ID") is not None)
 
 url = "https://api.mercadolibre.com/oauth/token"
 
 data = {
     "grant_type": "refresh_token",
-    "client_id":  os.environ["ML_CLIENT_ID"],
+    "client_id": os.environ["ML_CLIENT_ID"],
     "client_secret": os.environ["ML_CLIENT_SECRET"],
     "refresh_token": os.environ["ML_REFRESH_TOKEN"]
 }
 
-r = requests.post(url, data=data)
-r.raise_for_status()
-res = r.json()
+response = requests.post(url, data=data)
+print("STATUS:", response.status_code)
+print("BODY:", response.text)
+
+response.raise_for_status()
+res = response.json()
 
 with open("token.json", "w") as f:
-    json.dump({
-        "access_token": res["access_token"],
-        "refresh_token": res["refresh_token"],
-        "updated_at": datetime.utcnow().isoformat()
-    }, f)
+    json.dump(res, f)
